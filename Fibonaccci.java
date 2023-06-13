337e8147a559f21cfd28c2c529ac335b81d512cb
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-13 23:06:45 +0530

    Error

--- Fibonaccci.java
+++ Fibonaccci.java
@@ -1,3 +1,5 @@
 public class Fibonaccci {
-    
+    public static void main(String[] args){
+    System.out.println("abc");
+}
 }
--- Prime.java
+++ Prime.java
@@ -3,24 +3,25 @@ public class Prime {
     public static void main(String[] args)
     {
         int i,c=0;
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter the value to be checked for prime");
-        int n=sc.nextInt();
-        for(i=2;i<n;i++)
-        {
-          if(n%i==0)
+        try (Scanner sc = new Scanner(System.in)) {
+          System.out.println("enter the value to be checked for prime");
+          int n=sc.nextInt();
+          for(i=2;i<n;i++)
           {
-            System.out.println("enterd numebr is not a prime number");
-            c=1;
+            if(n%i==0)
+            {
+              System.out.println("enterd numebr is not a prime number");
+              c=1;
+            }
+
           }
+          if(c==0)
+          {
+              System.out.println("enterd number is a prime number");
 
+          }
+          sc.close();
         }
-        if(c==0)
-        {
-            System.out.println("enterd number is a prime number");
-
-        }
-        sc.close();
 
     }
 
