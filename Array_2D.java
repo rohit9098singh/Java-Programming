87b2dc469be0a22a2e5921bec18119b915c7cbb4
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-18 22:35:26 +0530

    2d array

---
+++ Array_2D.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Array_2D {
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+       System.out.println("enter the number of rows of your choice");
+       int r=sc.nextInt();
+       System.out.println("enter the number of column of your choice");
+       int c=sc.nextInt();
+       int a[][]=new int[r][c];
+       System.out.println("enter the array element of your choice");
+       for(int i=0;i<r;i++)
+       {
+        for( int j=0;j<c;j++)
+        {
+           a[i][j]=sc.nextInt();
+
+        }
+       }
+       System.out.println("your entetrd array is");
+       System.out.println("=====================");
+       for(int i=0;i<r;i++)
+       {
+        for(int j=0;j<c;j++)
+        {
+            System.out.println(a[i][j]+"\t");
+        }
+            System.out.print("\n");
+       }
+
+    }
+    
+}
