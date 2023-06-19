41a89faded57a359c9b2b4bde7537b7c2bd34c58
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-19 20:05:27 +0530

    loop change

--- Matrixadd.java
+++ Matrixadd.java
@@ -2,84 +2,68 @@ import java.util.Scanner;
 public class Matrixadd {
     public static void main(String[] args)
     {
-        int p,q,m,n;
+        int row,col;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number of rows of first mat");
-        p=sc.nextInt();
-        System.out.println("eneter the number of column of first mat");
-        q=sc.nextInt();
-        System.out.println("enter the number of rows of second mat");
-        m=sc.nextInt();
-        System.out.println("enter the number of column of second matrix");
-         n=sc.nextInt();
-        if(p==m && q==n)
+        System.out.println("enter  the row");
+        int r=sc.nextInt();
+        System.out.println("enter  the column");
+        int c=sc.nextInt();
+        int mat1[][]=new int[r][c];
+        int mat2[][]=new int[r][c];
+        int res[][]=new int[r][c];
+        System.out.println("enter the matrix one elements");
+        for(int i=0;i<r;i++)
         {
-            int a[][]=new int[p][q];
-            int b[][]=new int[m][n];
-            int c[][]=new int [m][n];
-            System.out.println("enter all the elements of the 1st matrix");
-            for(int i=0;i<p;i++)
+            for(int j=0;j<c;j++)
             {
-                for(int j=0;j<q;j++)
-                {
-                    a[i][j]=sc.nextInt();
-                }
+           mat1[i][j]=sc.nextInt();
             }
-              System.out.println("");
-            System.out.println("enter the elements of your second matrix");
-            for(int i=0;i<m;i++)
+        }
+        System.out.println("enter the matrix two elements");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
             {
-                for(int j=0;j<n;j++)
-                {
-                    b[i][j]=sc.nextInt();
-                }
+           mat2[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("your first matrix");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+              System.out.printf("%d\t",mat1[i][j]);
             }
             System.out.println("");
-            System.out.println("your first matrix");
+        }
+        System.out.println("your second matrix is");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+              System.out.printf("%d\t",mat2[i][j]);
 
-            for(int i=0;i<p;i++)
-            {
-                for(int j=0;j<q;j++)
-                {
-                    System.out.println(a[i][j]+"");
-                }
-                System.out.println("");
             }
-            System.out.println("your second matrix is");
-            for(int i=0;i<m;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    System.out.println(b[i][j]+"");
-                }
-               System.out.println(""); 
-            }
-            for(int i=0;i<p;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    for(int k=0;k<q;k++)
-                    {
-                     c[i][j]=a[i][j]+b[i][j];   
-                    }
-                }
-            }
-            System.out.println("matrix after addition---");
-            for( int i=0;i<p;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    System.out.println(c[i][j]+"");
-                }
             System.out.println("");
+        }
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+              res[i][j]=mat1[i][j]+mat2[i][j];
+
             }
-
         }
-        else{
-            System.out.println("invalid choice");
+        System.out.println("=====================");
+        System.out.println("your resultant matrix is ");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+              System.out.printf("%d\t",res[i][j]);
+            }
+            System.out.println("");    
         }
-
-
 
     }
-}
+}            
