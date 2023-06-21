dfaa0ec8b54dc283ad0e525542b5fff20e2c0014
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-21 20:30:58 +0530

    dupicate

---
+++ Duplicate.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Duplicate {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in); 
+        int search,c=0;
+        System.out.println("enter the size of the array");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("enter the array element");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println("eneter the element to be searched");
+        search=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==search)
+            {
+                System.out.printf("element found at index %d and the element is %d\n",i+1,search);
+                c=1;
+            }
+        }    
+            if(c==0)
+            {
+                System.out.println("element not found in the list");
+            }
+        
+
+    }
+    
+}
