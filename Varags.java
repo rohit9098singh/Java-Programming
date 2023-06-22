2a5b28ea43bbd86a2b09765023b78bb2f773aa6a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-22 19:38:10 +0530

    varags

--- Varags.java
+++ Varags.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class Varags {
+    float average(float...arr)
+    {
+       float total =0;
+       for(float e:arr)
+       {
+       total =total+e;
+       }
+    float average=total/arr.length;
+    return average;
+    }
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("eneter how many elements do u want");
+        int n=sc.nextInt();
+        float arr[]=new float[n];
+        System.out.println("eneter your elements");
+        for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextFloat();
+        }
+        Varags obj= new Varags();
+       System.out.println("average is:"+obj.average(arr));
+    }
     
 }
