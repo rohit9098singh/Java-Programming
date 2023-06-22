8b7f51419ec83c7f0239597e6fa54848350a50fa
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-22 18:59:39 +0530

    overloading

--- Method_overloading.java
+++ Method_overloading.java
@@ -2,34 +2,22 @@ import java.util.Scanner;
 
 import javax.crypto.interfaces.DHPrivateKey;
 public class Method_overloading {
-    void add()
+    void add(int a ,int b)
     {
-        Scanner sc=new Scanner(System.in);
-        int a,b;
-        System.out.println("eneter the value of a ,b");
-        a=sc.nextInt();
-        b=sc.nextInt();
+        
         int c=a+b;
         System.out.println("the sum of the two number are "+c);
     }
     void add(int a,int b,int c)
     {
-        Scanner sc=new Scanner(System.in);
-         System.out.println("eneter the value of a,b,c");
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
+        
         int d=a+b+c;
         System.out.println("the sum of the three number are "+d);
     }
     void add(int a,int b,int c,int d)
     {
         Scanner sc=new Scanner(System.in);
-         System.out.println("eneter the value of a,b,c,d");
-        a=sc.nextInt();
-        b=sc.nextInt();
-        c=sc.nextInt();
-        d=sc.nextInt();
+       
         int e=a+b+c+d;
         System.out.println("the sum of the four number are "+e);
 
@@ -38,8 +26,13 @@ public class Method_overloading {
     {
         Scanner sc=new Scanner(System.in);
         int a,b,c,d;
+          System.out.println("eneter the value of a,b,c,d");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        d=sc.nextInt();
         Method_overloading obj=new  Method_overloading();
-        obj.add();
+        obj.add(a,b);
         obj.add(a,b,c);
         obj.add(a,b,c,d);
         
