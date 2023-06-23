d5e6604637fbee259c0d257b8c5626b37da7be3d
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-23 22:49:04 +0530

    1st accessmod prg

---
+++ ch3_oops6_accesssmodifier.java
@@ -0,0 +1,35 @@
+class staff
+{
+    private int id;
+    private String name;
+    public void setname(String n)
+    {
+       name=n;  
+    }
+    public String getname()
+    {
+        return name;
+    }
+    public void setid(int i)
+    {
+        id=i;
+    }
+    public int getid()
+    {
+        return id;
+    }
+}
+public class ch3_oops6_accesssmodifier {
+    public static void main(String[] args)
+{
+    staff st=new staff();
+    /*  st.id=54;    
+    st.name="rohit"*/// both gives an error since the id and name are the pjrivate members cannot be accessed directly without a method
+
+    st.setid(+1);
+    st.setname("rohit");
+    System.out.println(st.getid());
+    System.out.println(st.getname());
+}
+    
+}
