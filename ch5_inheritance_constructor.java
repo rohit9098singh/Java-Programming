76d209f753bb3ec4e99f5cfca0d7df4d9ea45831
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-25 13:07:49 +0530

    inheritance 3rd

--- ch5_inheritance_constructor.java
+++ ch5_inheritance_constructor.java
@@ -7,9 +7,9 @@ class base
     {
         System.out.println("i am a normal base class constructor");
     }
-    base(int x)
+    base(int a)
     {
-        System.out.println("i am an base class overloaded constructor with the vlaue of a is="+x);
+        System.out.println("i am an base class overloaded constructor with the vlaue of a is="+a);
     }
 
 }
@@ -24,13 +24,25 @@ class child1 extends base{
         super(a);
         System.out.println("i am an overloaded child class constructor with the value of b="+b);
     }
-
-    
-
 }
+
+  class grandchild extends child1
+  {
+    int c=7*8;
+    grandchild()
+    {
+        System.out.println("i am an normal constructor of grandchld with the vakue of c="+c);
+    }
+    grandchild(int a,int b,int c)
+    {
+        super(a,b);
+        System.out.println("i am an overloaded constructor with the value of  c is="+c);
+    }
+  }
 public class ch5_inheritance_constructor {
     public static void main(String[] args) {
-        child1 ch=new child1(20,30);
+       // grandchild ch=new grandchild();
+        grandchild ch=new grandchild(20,49,75);
     }
     
 }
