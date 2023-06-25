06a4e156be100ce4d4d2be92bd1de78ae3786baf
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-25 16:00:49 +0530

    inhritance-4

---
+++ ch5_inhrtitance_methodoveriding.java
@@ -0,0 +1,37 @@
+class A
+{
+    public int a;
+    public int display()
+    {
+        return 4;
+
+    }
+    public void meth2()
+    {
+        System.out.println("i am method 2 of class a");
+    }
+}
+class B extends A
+{
+     @Override
+    public void meth2()
+    {
+        System.out.println("i am  method 2 of class b");
+
+    }
+    public void meth3()
+    {
+        System.out.println("i am method 3 of class b");
+    }
+}
+public class ch5_inhrtitance_methodoveriding {
+    public static void main(String[] args) {
+        A a=new A();
+        a.meth2();
+        B b=new B();
+        b.meth2();
+        b.meth3();
+
+    }
+    
+}
