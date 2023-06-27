bd38842dc4043f2549f61d9d4a8b1da913a7202f
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-06-27 23:35:24 +0530

    new

---
+++ ch7_abstractclass.java
@@ -0,0 +1,33 @@
+abstract class Sum {
+    public abstract int sumTwo(int a, int b);
+
+    public abstract int sumThree(int a, int b, int c);
+
+    void display() {
+        System.out.println("This is abstract Class");
+    }
+
+}
+
+class demo extends Sum {
+
+    @Override
+    public int sumTwo(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int sumThree(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
+public class ch7_abstractclass {
+    public static void main(String[] args) {
+        demo d =new demo();
+        System.out.println(d.sumTwo(2,66));
+}
+}
+
+
