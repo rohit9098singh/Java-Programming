a1d35cea60c1ca8214e5dbffe43cb34794a3782c
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-10 20:15:26 +0530

    daemon method

---
+++ ch12_2demonthread.java
@@ -0,0 +1,44 @@
+class demonexamlpe implements Runnable
+{
+    public void run()
+    {
+        int i=0;
+        while(i<10)
+        {
+            try
+            {
+            System.out.println("demon thread executing here ");
+            Thread.sleep(1000);
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+        System.out.println("demon thread task is over");
+    }
+}
+public class ch12_2demonthread {
+    public static void main(String[] args) {
+        demonexamlpe d =new demonexamlpe();
+        Thread t=new Thread(d);
+        t.setDaemon(true);
+        t.start();
+
+        for(int i=0;i<5;i++)
+        {
+            try{
+                System.out.println("main thread task");
+                Thread.sleep(1000);
+            }
+                  catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            
+        }
+        System.out.println("main thread task is over");
+
+    }
+    
+}
