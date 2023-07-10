46256ad0eba4294ee972ab186db90cff3aac7bd9
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-10 22:42:17 +0530

    changes

--- ch12_priority_thread.java
+++ ch12_priority_thread.java
@@ -7,13 +7,14 @@ class work extends Thread
 }
 public class ch12_priority_thread {
     public static void main(String[] args) {
-        work w= new work();
-        w.start();
-        System.out.println("main thread original priority :"+Thread.currentThread().getPriority());
+          System.out.println("main thread original priority :"+Thread.currentThread().getPriority());
+          
+         Thread.currentThread().setPriority(10);
+         System.out.println("main thread changed priority:"+Thread.currentThread().getPriority());
 
+         work w= new work();
+          w.start();
 
-        Thread.currentThread().setPriority(10);
-        System.out.println("main thread changed priority:"+Thread.currentThread().getPriority());
     
     }
     
