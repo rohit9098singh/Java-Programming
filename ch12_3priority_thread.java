2f694190b3924b746fb301295a211fd6421a5dfc
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-10 23:49:11 +0530

    name change

--- ch12_3priority_thread.java
+++ ch12_3priority_thread.java
@@ -9,7 +9,7 @@ public class ch12_3priority_thread {
     public static void main(String[] args) {
           System.out.println("main thread original priority :"+Thread.currentThread().getPriority());
           
-         Thread.currentThread().setPriority(10);
+         Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
          System.out.println("main thread changed priority:"+Thread.currentThread().getPriority());
 
          work w= new work();
@@ -19,3 +19,9 @@ public class ch12_3priority_thread {
     }
     
 }
+/*NOTE FOR 
+   MAX_PRIORITY
+ * MIN_PRIORITY
+ * NORM_PRIORITY
+ * SHOULD BE WRITTEN IN CAPS
+ */
