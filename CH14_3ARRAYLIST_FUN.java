1785f681be1e5cf1fa42e645f8a691521a8d6caf
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-12 00:55:20 +0530

    modification

--- CH14_3ARRAYLIST_FUN.java
+++ CH14_3ARRAYLIST_FUN.java
@@ -1,92 +1,77 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
+
 public class CH14_3ARRAYLIST_FUN {
     public static void main(String[] args) {
-        ArrayList <Integer> integers=new ArrayList<>();
-        Scanner sc=new Scanner(System.in);
-         
-        while(true)
-         {
-            System.out.println("\nENTER YOUR CHOICE\n1:ADD\n2:REMOVE\n3:REPLACE\n4:sort\n5:delete_everything\n6:display\n7:exit");
-             int choice=sc.nextInt();
-             System.out.println("*************");
+        ArrayList<Integer> integers = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-          switch(choice)
-             {
-                case 1:
-                {
-                   System.out.println("enter the number of element you want to add");
-                   int n=sc.nextInt();
-                   for(int i=0;i<n;i++)
-                     {
-                       System.out.println("enter your "+i+"element");
-                       int s=sc.nextInt();
-                       integers.add(s);
-                    
-                     }
-                     System.out.println(integers);
-                      break;
+        while (true) {
+            System.out.println(
+                    "\nENTER YOUR CHOICE\n1:ADD\n2:REMOVE\n3:REPLACE\n4:sort\n5:delete_everything\n6:display\n7:exit");
+            int choice = sc.nextInt();
+            System.out.println("*************");
+
+            switch (choice) {
+                case 1: {
+                    System.out.println("enter the number of element you want to add");
+                    int n = sc.nextInt();
+                    for (int i = 0; i < n; i++) {
+                        System.out.println("enter your " + i + " element");
+                        int s = sc.nextInt();
+                        integers.add(s);
+
+                    }
+                    System.out.println(integers);
+                    break;
                 }
-                case 2:
-                {
+                case 2: {
                     System.out.println("enyter the element you want to remove");
-                    int e=sc.nextInt();
-                    if(integers.contains(e))
-                    {
+                    int e = sc.nextInt();
+                    if (integers.contains(e)) {
                         integers.remove(Integer.valueOf(e));
-                    } 
-                    else
-                    {
+                    } else {
                         System.out.println("elemet not found");
-                    }   
-                        System.out.println(integers);
-                        break;
-                    
-                }
-                case 3:
-                {
-                    System.out.println("enter the index u want to replace");
-                    int ind=sc.nextInt();
-                    System.out.println("enter the value u want to replace");
-                    int val=sc.nextInt();
-                    integers.set(ind,val);
-                    
+                    }
+                    System.out.println(integers);
+                    break;
 
                 }
-                case 4:
-                {
+                case 3: {
+                    System.out.println("enter the index u want to replace");
+                    int ind = sc.nextInt();
+                    System.out.println("enter the value u want to replace");
+                    int val = sc.nextInt();
+                    integers.set(ind, val);
+                    break;
+
+                }
+                case 4: {
                     Collections.sort(integers);
                     System.out.println(integers);
+                    break;
                 }
-                case 5:
-                {
+                case 5: {
                     integers.clear();
                     System.out.println(integers);
-                    
+                    break;
+
                 }
-                case 6:
-                {
-                    System.out.println("LIST"+integers);
+                case 6: {
+                    System.out.println("LIST" + integers);
+                    break;
                 }
-                case 7:
-                {
-                  System.out.println("*******thanks for using*******");
+                case 7: {
+                    System.out.println("*******thanks for using*******");
                 }
-                
+
             }
-            if(choice==7)
-            {
+            if (choice == 7) {
                 break;
             }
-             
-
 
         }
-        
-    
 
     }
- }
-    
-
+}
