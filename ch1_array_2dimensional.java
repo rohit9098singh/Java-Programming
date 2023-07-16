4c9ddca0a180198652f1673bffca224312b02131
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-16 23:04:40 +0530

    array

--- ch1_array_2dimensional.java
+++ ch1_array_2dimensional.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Array_2D {
+public class ch1_array_2dimensional{
     public static void main(String[] args)
     {
         Scanner sc =new Scanner(System.in);
