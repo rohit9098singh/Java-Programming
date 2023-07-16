b98b2ad1c96b4c70e74f46afda4de7b7d3257455
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-17 00:23:35 +0530

    lab 4

---
+++ LAB_4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class payOutOfBounds extends Exception
+{
+    payOutOfBounds(String msg)
+    {
+        System.out.println("pay out of bound exception "+msg);
+    }
+}
+public class LAB_4 {
+    public static void main(String[] args) throws payOutOfBounds  {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the employee salary");
+        int pay=sc.nextInt();
+        if(pay<10000||pay>50000)
+        {
+            throw new payOutOfBounds("slary not in a valid range");
+            
+        }
+        else{
+            System.out.println("employee is eligible for 30 percent hike");
+        }
+        
+        
+        
+    }
+    
+}
