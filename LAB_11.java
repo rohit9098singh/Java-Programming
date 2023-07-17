3be9c3ea578313001ed7650439aef31223859b29
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-17 19:57:13 +0530

    lab 11

---
+++ LAB_11.java
@@ -0,0 +1,10 @@
+public class LAB_11 {
+    public static void main(String[] args) {
+        try {
+            int arr[] = new int[-1];
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
