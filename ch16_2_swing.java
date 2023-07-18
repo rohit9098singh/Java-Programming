97788ae40f18f8a6d0f9fa3612a08f512a3cd710
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-18 19:19:15 +0530

    swing 2

--- ch16_2_swing.java
+++ ch16_2_swing.java
@@ -4,15 +4,21 @@ import javax.swing.JFrame;
 class dframe extends JFrame{
     dframe()
     {
+   /*Panel p=new Panel(); *//*IF WE DONT MAKE THE PANNEL OBJECT AND DONT USE P.ADD(B) THEN THE BUTTON AND THAT ARE NOT GOING TO BE 
+    BORDERED MEANS IT WILL NOT SEEM LIKE IT IS A BUTTON JUST A SIMPLE WRITING WILL BE THERE COMMENT THIS AND USE THE ALREADY 
+    COMMENTED PART TO SEE THE EXPLAINATION */
     Button b=new Button("submit");
-    add(b);
+    //p.add(b);
+   // add(p);
+
+     add(b);//comment this and uncooment the commented part to see transaltion
     setSize(400,400);
     setTitle("my frame");
     setVisible(true);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
-public class ch16_3_swing {
+public class ch16_2_swing {
     public static void main(String[] args) {
         dframe d=new dframe();
     }
---
+++ ch16_3_swing.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import javax.swing.*;
+class pannelex extends JFrame{
+    pannelex()
+    {
+        /*IT IS BASICALLY THE TEMPLET OF THE GUI WHICH EVERY PROGRAMME SHOULD NEARLY HAVE IT */
+        setTitle("cromepage");
+        setSize(400,400);
+        setMinimumSize(new Dimension(300, 400));
+         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        /*NOW HERE WE ARE ACTUALLY DOING THINGS THAT WE ACTUALLY WANT IN OUR GUI TO BE PRESENT */
+
+        setLayout(new GridBagLayout());/*USED FOR BORDERING MEANS HOW THE BUTTONS PANNELS AND ALL WILL BE ARRANGED IN OUR INTERFACE 
+        BASICALLY THERE ARE THREE TYPES OF LAYOUT SYSTEM THAT WE CAN USE 1.ALREADY USED 2-BorderLayout 3-FlowLayout EVERYONE HAS ITS 
+        OWN SPECIFIC MEANING*/
+        Panel p=new Panel();/*THEY ARE THE USED FOR INNER GRID OR BORDER TYPE IN THE GUI WHICH IS BUILD IN THE PROGRAMME WHICH WE CAN USE AS PUUTING SOME DATA IN IT */
+        Label l1=new Label("panel 1");/*JLabel can display text, image or both . JLabel is only a display of text or image .and it cannot get focus . JLabel is inactive to input events such a mouse focus or keyboard focus. By default labels are vertically centered but the user can change the alignment of label. */
+        p.add(l1);
+        Button b1=new Button("button 1");
+        p.add(b1);
+        add(p);
+
+        /*HERE WE HAVE CREATED ANOTHER PANNEL HERE */
+        Panel p2=new Panel();
+        Label l2=new Label("pannel 2");
+        Button b2=new Button("button 2");
+        p2.add(l2);
+        p2.add(b2);
+        add(p2);
+
+
+
+
+    }
+}
+public class ch16_3_swing {
+    public static void main(String[] args) {
+        pannelex pan=new pannelex();
+    }
+    
+}
