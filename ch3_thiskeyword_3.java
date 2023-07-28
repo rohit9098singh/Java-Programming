850d8a311fc4bb10c85adae5d431d83e31f04672
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-07-28 15:22:58 +0530

    first this

---
+++ ch3_thiskeyword_3.java
@@ -0,0 +1,22 @@
+class kgfmines
+{
+    public int id;//instance variable
+    public String name;
+    kgfmines(int id,String name)//passed argument are the local variable
+    {
+        this.id=id;
+        this.name=name;
+    }
+    void showfunction()
+    {
+        System.out.println("the enterd id is "+id);
+        System.out.println("the eneted name is "+name);
+    }
+}
+public class ch3_thiskeyword_3 {
+    public static void main(String[] args) {
+        kgfmines kgf=new kgfmines(1, "rahul");
+        kgf.showfunction();
+    }
+    
+}
