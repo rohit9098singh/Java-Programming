ac5cc390db0087ad740ea13666e69048081a101e
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-12 00:33:29 +0530

    slight

--- ch18_now.java
+++ ch18_now.java
@@ -1,7 +1,7 @@
 public class ch18_now {
     public static void main(String[] args) {
         int a = 20, b = 30, c = 5;
-        if (a > b) {
+        if (a > b) {//return type of if statement are boolean because it checks for true or fale
             if (a > c) {
                 System.out.println("a is bigger");
 
