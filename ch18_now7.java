f46cb134aeddc3349e01fe94686e69f8fe26e54c
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-12 19:33:54 +0530

    one kind of sorting

---
+++ ch18_now7.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ch18_now7{
+    public static void main(String[] args) {
+        String nums []=new String[]{"10","21","01"};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
--- pracriceset.java
+++
@@ -1,6 +0,0 @@
-public class pracriceset
-{
-    public static void main(String[] args) {
-        Thread t=new Thread(new myRunnable());
-    }
-}
