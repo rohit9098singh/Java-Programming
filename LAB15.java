529d6361ea8ac1429dc41beccd52c735f9c45e29
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-21 18:31:53 +0530

    lab

---
+++ LAB15.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+public class LAB15 extends Canvas {
+          public void paint(Graphics g)
+         {
+           g.drawRect(50,75,100,50);
+           g.fillRect(50,75,100,50);
+           g.drawRoundRect(50,150,100,50,15,15);
+           g.fillRoundRect(50,150,100,50,15,15);
+           g.drawOval(50,275,100,50);
+           g.fillOval(50,275,100,50);
+           g.drawArc(20,350,100,50,25,75);
+           g.fillArc(20,350,100,50,25,75);
+         }
+        public static void main(String[] args) {
+            LAB15 demo=new LAB15();
+            Frame f=new Frame();
+            f.add(demo);
+            f.setVisible(true);
+            f.setSize(300,400);
+
+        }
+    
+
+    
+    
+}
--- LAB_13.java
+++ LAB_13.java
@@ -1,38 +1,20 @@
-import javax.swing.*;
 import java.awt.*;
-
- class MessageDisplay extends JFrame {
-
-    public MessageDisplay() {
-                                        
-        setTitle("Message Window");//Set the window title
-      setSize(300, 200); // Set the size of the window                                
-       Panel p = new Panel();// Create a panel to hold the message label
-       Label message = new Label("Hello, this is a message!");// Create a label with the message to be displayed
-    
-       p.add(message);// Add the label to the panel
-       
-       getContentPane().add(p); // Add the panel to the content pane of the frame
-        
-       // Set the default close operation
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        // Make the window visible
-        setVisible(true);
-        
-
-   }
- }    
-    public class LAB_13
+class framedemo {
+    framedemo()
     {
-      public static void main(String[] args) {
-        // Create an instance of the MessageDisplay class to display the window
-        MessageDisplay d= new MessageDisplay();
+        Frame f=new Frame();
+        f.setTitle("first gui programme");
+       // Panel p=new Panel();
+        Label l=new Label("welcome to gui interface happy to see u here dear");
+        f.setBounds(50,75,350,50);
+        f.add(l);
+       f. setVisible(true);
+        f.setSize(300,300);
+        f.setLayout(null);
     }
-    
+}
+public class LAB_13 {
+    public static void main(String[] args) {
+     framedemo d=new framedemo();   
     }
-    /*<Applet code=Myapplet.class width=400 height=500>
-</Applet>
-*/
-
-
+}
