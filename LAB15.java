4c9538d9505b0ca1408232e5ec36fc0fefc1e9e5
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-21 19:22:36 +0530

    lab programme

--- LAB15.java
+++ LAB15.java
@@ -1,26 +1,51 @@
 import java.awt.*;
-public class LAB15 extends Canvas {
-          public void paint(Graphics g)
-         {
-           g.drawRect(50,75,100,50);
-           g.fillRect(50,75,100,50);
-           g.drawRoundRect(50,150,100,50,15,15);
-           g.fillRoundRect(50,150,100,50,15,15);
-           g.drawOval(50,275,100,50);
-           g.fillOval(50,275,100,50);
-           g.drawArc(20,350,100,50,25,75);
-           g.fillArc(20,350,100,50,25,75);
-         }
-        public static void main(String[] args) {
-            LAB15 demo=new LAB15();
-            Frame f=new Frame();
-            f.add(demo);
-            f.setVisible(true);
-            f.setSize(300,400);
+import java.awt.event.*;
+public class LAB15  {
+    public static void main(String[] args) {
+        Frame f=new Frame();
+        f.setTitle("button example");
+        f.setSize(300,400);
+        f.setVisible(true);
+        Label l=new Label("Details of the parents");
+        l.setFont(new Font("Calibri",Font.BOLD,16));
+        Label l1=new Label();
+        Label l2=new Label();
+        Label l3=new Label();
+        l.setBounds(20, 20, 500, 50);
+         l1.setBounds(20, 110, 500, 30);
+       l2.setBounds (20, 150, 500, 30);
+       l3.setBounds(20, 190, 500, 30);
+        Button b=new Button("mother");
+        b.setBounds (20, 70, 50, 30);
+        Button b2=new Button("father");
+        b2.setBounds (80, 70, 50, 30);
 
-        }
-    
+        b.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          {
+            l1.setText("name="+""+ "sushila");
+            l2.setText("designation"+""+"profressor of maths in acharya");
+            l3.setText("age:"+""+"45");
+          }
+        });
+        b2.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                l1.setText("name ="+""+"ramesha");
+                l2.setText("designatio"+""+"house wife");
+                l3.setText("age"+""+"34");
+            }
 
+        });
+        f.add(l1);
+        f.add(l2);
+        f.add(l3);
+        f.add(b);
+        f.add(b2);
+        f.setLayout(null);
     
+    }
     
 }
