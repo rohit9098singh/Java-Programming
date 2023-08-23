d52e9506cfbe4fa3695dd5f8fef3275073a4565c
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-24 01:25:13 +0530

    haha

--- ch18_now21.java
+++ ch18_now21.java
@@ -1,26 +1,26 @@
-class staff
+class mansion
 {
     String name,phoneno;  
     int id,salary;  
-    public staff(int id,String name,int salary, String phoneno)  
+    public mansion(int id,String name,int salary, String phoneno)  
     { 
         this.salary=salary;  
         this.phoneno=phoneno;  
         this.id=id;  
         this.name=name;  
     } 
-   public  staff(int id,String phoneno)  
+   public  mansion(int id,String phoneno)  
     { 
         this.id=id; 
         this.phoneno=phoneno; 
     } 
-    public staff(String name,int salary,String phoneno) 
+    public mansion(String name,int salary,String phoneno) 
     { 
         this.name=name; 
         this.salary=salary; 
         this.phoneno=phoneno; 
     } 
-    public staff(int id,String name,String phoneno) 
+    public mansion(int id,String name,String phoneno) 
     {
         this.id=id;
         this.name=name;
@@ -28,19 +28,20 @@ class staff
     }
     public void display()
     {
-        System.out.println("id\t\tname\t\tsalary\t\tphoneno");
+        
         System.out.println(id+"\t\t"+name+"\t\t"+salary+"\t\t"+phoneno);
     }
 }
 public class ch18_now21 {
     public static void main(String[] args) {
-        staff s=new staff(1,"ram",1000,"95724397");
+        System.out.println("id\t\tname\t\tsalary\t\tphoneno");
+        mansion s=new mansion(1,"ram",1000,"95724397");
         s.display();
-        staff s1=new staff(2,"9572439");
+        mansion s1=new mansion(2,"9572439");
         s1.display();
-        staff s2=new staff("shyam",20000,"9572439");
+        mansion s2=new mansion("shyam",20000,"9572439");
         s2.display();
-        staff s3=new staff(3,"10000","95724397");
+        mansion s3=new mansion(3,"10000","95724397");
         s3.display();
         
     }
