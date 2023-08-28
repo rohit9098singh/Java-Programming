92211e5ab7df3a46d08e298b75b35bdbdba3366a
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-28 23:43:52 +0530

    now

--- ch18_now27.java
+++ ch18_now27.java
@@ -5,10 +5,13 @@ public class ch18_now27 {
             for(int j=0;j<=3;j++)
             {
                 if(j==1)
+                {
                 System.out.println();
                 continue;
+                }
+            
+              System.out.print(i);
             }
-            System.out.println(i);
         }
         
     }
