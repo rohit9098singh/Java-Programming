b563049885287440eb19cb864caba944151cafc3
rohit@ROHIT <rohitsingh95724@gmail.com>
2023-08-28 23:39:01 +0530

    now 27

---
+++ ForEx.java
@@ -0,0 +1,16 @@
+class ForEx{
+    public static void main(String args[]){
+        String s="IITMadras";
+        int i=0;
+    
+       /*  for(i=0;s!=null;i++){
+            
+        }
+        System.out.println(i);*/
+        while(s!=null)
+        {
+            i++;
+        }
+        System.out.println(i);
+        }
+}
---
+++ ch18_now27.java
@@ -0,0 +1,16 @@
+public class ch18_now27 {
+    public static void main(String[] args) {
+        for(int i=0;i<=3;i++)
+        {
+            for(int j=0;j<=3;j++)
+            {
+                if(j==1)
+                System.out.println();
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+    }
+    
+}
